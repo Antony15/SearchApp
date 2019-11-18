3aa96fffcb1d0e06e4631ad317ebdc4ce632bbe5
Antony15 <aanto157@gmail.com>
2019-11-18 11:46:38 +0530

    Add main.go

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
+ 
 package main
+ 
+import (
+"fmt"
+"time"
+"net/http"
+"io/ioutil"
+"encoding/json"
+"github.com/karlseguin/ccache"
+)
 
-import "fmt"
+var cache = ccache.New(ccache.Configure().MaxSize(5))
 
 func main() {
-    fmt.Println("Hello, World")
+
+	var data map[string]interface{}
+	item := cache.Get("tata motors")
+	fmt.Println(item)
+	if item == nil {
+		url := "https://api.duckduckgo.com/?q=tata motors&format=json"
+		 
+		req, _ := http.NewRequest("GET", url, nil)
+		 
+		res, _ := http.DefaultClient.Do(req)
+		 
+		defer res.Body.Close()
+
+		body, _ := ioutil.ReadAll(res.Body)
+
+		if err := json.Unmarshal(body, &data); err != nil {
+			panic(err)
+		}
+		cache.Set("tata motors", body, time.Minute * 10)
+		
+		fmt.Println(data["Heading"])
+		fmt.Println(data["AbstractURL"])
+		fmt.Println(data["AbstractText"])
+	} else {
+	  //~ data := item.Value().(*User)
+	  fmt.Println("no")
+	}	 
+	
+
 }
